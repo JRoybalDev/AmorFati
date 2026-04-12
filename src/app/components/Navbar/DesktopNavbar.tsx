f52ef76778167993fc6bd1164b94d866d5a609dd
ExcuseMeImJack <excusemeimjack@gmail.com>
2026-04-12 10:28:21 -0700

    Navbar age removal

--- src/app/components/Navbar/DesktopNavbar.tsx
+++ src/app/components/Navbar/DesktopNavbar.tsx
@@ -22,7 +22,7 @@ function DesktopNavbar({ navItems, currPath }: NavItems) {
       {/* Hero */}
       <div className="pb-4">
         <Link className="text-4xl font-medium font-manufacturingConsent" href={"/"}>Amor Fati</Link>
-        <h3 className="text-h2 font-pirataOne font-normal">28 | I enjoy staring at the sky and singing to the ocean.</h3>
+        <h3 className="text-h2 font-pirataOne font-normal">I enjoy staring at the sky and singing to the ocean.</h3>
       </div>
 
       <div className='border-b-6 rounded-lg border-BGdivider' />
